93192268cf391198963ff3cb414b186c1e811631
AutoJenny <github@nickfiddes.com>
2025-04-23 10:29:43 +0100

    delettions

--- posts/posts.11tydata.js
+++
@@ -1,6 +0,0 @@
-// posts/posts.11tydata.js
-module.exports = {
-    layout: "post.njk",
-    permalink: "/{{ page.fileSlug }}/index.html",    // --- ADD THIS LINE ---
-    sourceInfo: "Set in posts.11tydata.js"
-  };
